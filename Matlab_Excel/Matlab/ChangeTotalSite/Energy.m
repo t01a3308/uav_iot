a7c12fb8730991fa30901250075472ccf9a70a5d
nguyen <nguyenpv195@gmail.com>
2020-06-19 13:52:03 +0700

    matlab + excel

--- Matlab_Excel/Matlab/ChangeTotalSite/Energy.m
+++ Matlab_Excel/Matlab/ChangeTotalSite/Energy.m
@@ -1,10 +1,8 @@
-x = [300 325 350 375 400 425 450 475 500];
-y1 = [11.2893	12.2621	13.0208	13.8127	14.7317	15.6287	16.3375	17.2102	18.0305]; % global tsp
-y2 = [4.60093	5.9503	7.17933	8.19474	9.77035]; % local tsp
-y3 = [12.7876	13.8515	14.6172	15.3537	16.337	17.1864	17.9918	18.7993	19.7254]; % iterative
-y4 = [11.9904	12.8398	13.6249	14.4109	15.2687	16.0314	16.8065	17.6816	18.4324]; % inter-cell
-y5 = [12.2691	13.4111	14.5695	15.7396	16.7564	17.9152	18.9904	20.283	21.3962];% GELSGA
-y6 = [11.2948	12.1561	12.9553	13.7563	14.6214	15.4302	16.2295	17.0878	17.8833]; %CVRP
+x = [300 350 400 450 500 550 600 650 700];
+y3 = [11.9142	13.1232	14.1822	15.785	17.5033	18.8977	20.1464	21.4732	23.38]; % iterative
+y4 = [10.4717	11.7788	13.1	14.5369	16.2007	17.9188	19.355	20.9836	22.4878]; % inter-cell
+y5 = [11.3049	13.1381	14.2954	15.5709	17.1588	18.9832	20.6334	22.7202	24.6916];% GELSGA
+y6 = [10.4922	11.9476	13.4144	15.0662	16.6269	18.2011	19.736	21.5841	23.2871]; %CVRP
 close all;
 figure1 = figure;
 % Create axes
@@ -12,17 +10,15 @@ axes1 = axes('Parent',figure1);
 plot(x,y4,'-kv', 'LineWidth',2);
 hold on;
 grid on;
-% plot(x,y2,'-bs', 'LineWidth',2);
 plot(x,y3,'-rd', 'LineWidth',2);
-% plot(x,y1,'-mo', 'LineWidth',2);
-% plot(x,y5,'-x','Color',[0 0.4 0], 'LineWidth',2);
+plot(x,y5,'-x','Color',[0 0.4 0], 'LineWidth',2);
 plot(x,y6,'->','Color',[1 0.5 0], 'LineWidth',2);
 xlabel('Total sites');
 ylabel('Energy consumption (MJ) ');
-legend('Inter-cell','Iterative strategy', 'CVRP');
+legend('Inter-cell','Iterative strategy', 'GELSGA', 'CVRP');
 set(axes1,'XColor',[0 0 0],'YColor',[0 0 0]);
-xticks([300 325 350 375 400 425 450 475 500]);
-xlim([300 500]);
+xticks([300 350 400 450 500 550 600 650 700]);
+xlim([300 700]);
 ytickformat('%,4.4g');
 xtickformat('%,4.4g');
 hold off;
--- Matlab_Excel/Matlab/ChangeTotalSite/FlightTime.m
+++ Matlab_Excel/Matlab/ChangeTotalSite/FlightTime.m
@@ -1,28 +1,25 @@
-x = [300 325 350 375 400 425 450 475 500];
-y1 = [8.71906	9.47039	10.0483	10.6537	11.3627	12.052	12.5888	13.2652	13.8919]; % global tsp
-y2 = [3.41307	4.45627	5.33061	6.08049	7.27282]; % local tsp
-y3 = [9.88282	10.7049	11.2905	11.8529	12.6111	13.2629	13.8798	14.4977	15.2114]; % iterative
-y4 = [9.25007	9.90207	10.5031	11.1047	11.7633	12.3464	12.9393	13.6106	14.1853]; % inter-cell
-y5 = [9.49767	10.3811	11.2792	12.1861	12.97	13.8684	14.6991	15.7006	16.5647];% GELSGA
-y6 = [8.69793	9.35923	9.97129	10.5852	11.2496	11.8691	12.4812	13.1394	13.7493]; %CVRP
+x = [300 350 400 450 500 550 600 650 700];
+y3 = [9.04309	9.93744	10.7126	11.911	13.2063	14.2453	15.1669	16.1436	17.5807]; % iterative
+y4 = [7.8982	8.87065	9.85705	10.9411	12.1909	13.4766	14.5484	15.7647	16.8867]; % inter-cell
+y5 = [8.55927	9.94734	10.8008	11.7399	12.9306	14.3088	15.5477	17.1243	18.614];% GELSGA
+y6 = [7.91476	9.00428	10.1029	11.3406	12.5098	13.6911	14.8383	16.2259	17.5042]; %CVRP
 close all;
 figure1 = figure;
 % Create axes
 axes1 = axes('Parent',figure1);
-plot(x,y4,'-kv', 'LineWidth',2);
+width = 2;
+plot(x,y4,'-kv', 'LineWidth',width);
 hold on;
 grid on;
-% plot(x,y2,'-bs', 'LineWidth',2);
-plot(x,y3,'-rd', 'LineWidth',2);
-% plot(x,y1,'-mo', 'LineWidth',2);
-% plot(x,y5,'-x','Color',[0 0.4 0], 'LineWidth',2);
-plot(x,y6,'->','Color',[1 0.5 0], 'LineWidth',2);
+plot(x,y3,'-rd', 'LineWidth',width);
+plot(x,y5,'-x','Color',[0 0.4 0], 'LineWidth',width);
+plot(x,y6,'->','Color',[1 0.5 0], 'LineWidth',width);
 xlabel('Total sites');
 ylabel('Flight time (h)');
-legend('Inter-cell','Iterative strategy', 'CVRP');
+legend('Inter-cell','Iterative strategy', 'GELSGA', 'CVRP');
 set(axes1,'XColor',[0 0 0],'YColor',[0 0 0]);
-xticks([300 325 350 375 400 425 450 475 500]);
-xlim([300 500]);
+xticks([300 350 400 450 500 550 600 650 700]);
+xlim([300 700]);
 ytickformat('%,4.4g');
 xtickformat('%,4.4g');
 hold off;
